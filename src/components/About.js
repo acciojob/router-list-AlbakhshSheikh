3d75c60303f1e722c4e7bfbf1eb471adc9d93c30
Albakhsh Sheikh <98633969+AlbakhshSheikh@users.noreply.github.com>
2025-05-30 22:32:08 +0530

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  );
+}
+
+export default About;
